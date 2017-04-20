d5ff09ede742480725101bb9495f2c20812b04a0
isaacrlee <isaacrlee@gmail.com>
2017-04-20 13:02:44 -0500

    Implemented tag filtering

--- org/OrgController.js
+++ org/OrgController.js
@@ -33,6 +33,15 @@ router.get('/', function (req, res) {
     })
 })
 
+// RETURNS ALL THE ORGANIZATIONS W/TAG IN THE DATABASE
+router.get('/:tag_slug', function (req, res) {
+    // router.get('/:tag_guid', function (req, res) {
+    Org.find({ tag_slug: { $eq: req.params.tag_slug } }, function (err, orgs) {
+        if (err) return res.status(500).send("There was a problem finding the organizations.");
+        res.status(200).send(orgs)
+    })
+})
+
 // GETS A SINGLE ORGANIZATION FROM THE DATABASE
 router.get('/:id', function (req, res) {
     Org.findById(req.params.id, function (err, org) {
@@ -46,14 +55,14 @@ router.get('/:id', function (req, res) {
 router.delete('/:id', function (req, res) {
     Org.findByIdAndRemove(req.params.id, function (err, org) {
         if (err) return res.status(500).send("There was a problem deleting the organization.");
-        res.status(200).send("Organization: "+ org.org_name +" was deleted.");
+        res.status(200).send("Organization: " + org.org_name + " was deleted.");
     });
 });
 
 // UPDATES A SINGLE ORGANIZATION IN THE DATABASE
 router.put('/:id', function (req, res) {
-    
-    Org.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, org) {
+
+    Org.findByIdAndUpdate(req.params.id, req.body, { new: true }, function (err, org) {
         if (err) return res.status(500).send("There was a problem updating the organization.");
         res.status(200).send(org);
     });

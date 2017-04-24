3c2e6dbeb2ee5f528d6d6e2823ae1f39aea15a9c
isaacrlee <isaacrlee@gmail.com>
2017-04-24 15:46:55 -0500

    Updated start script

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-// const path = require('path')
-const express = require('express')
-// const exphbs = require('express-handlebars')
-// const jsonfile = require('jsonfile')
-
 var app = require('./app');
 var port = process.env.PORT || 5000;
 
-var server = app.listen(port, function() {
+app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
